c436952df36ad1e9f6b851014f803efecc567438
dtrix90 <dominique_90_@hotmail.com>
2023-02-12 16:07:50 +0100

    initial commit
    top nav bar, routes

---
+++ angular-web/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { DevicesComponent } from './devices/devices.component';
+import { GroupsComponent } from './groups/groups.component';
+import { RoutinesComponent } from './routines/routines.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CreateaccountComponent } from './createaccount/createaccount.component';
+
+const appRoute: Routes = [
+  {path:'Home', component: HomeComponent},
+  {path:'Devices', component: DevicesComponent},
+  {path:'Groups', component: GroupsComponent},
+  {path:'Routines', component: RoutinesComponent},
+  {path:'Aboutus', component: AboutusComponent},
+  {path:'Login', component: LoginComponent},
+  {path:'', redirectTo: 'Login', pathMatch: 'full'}
+  //{path:'', component: HomeComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DevicesComponent,
+    GroupsComponent,
+    RoutinesComponent,
+    AboutusComponent,
+    LoginComponent,
+    CreateaccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    RouterModule.forRoot(appRoute)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

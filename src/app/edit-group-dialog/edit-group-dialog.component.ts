65f24fdb4b12dd022a674cb93f1c61a9944a36cf
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-04-23 19:24:05 +0200

    Group name and description can now be edited.

--- src/app/edit-group-dialog/edit-group-dialog.component.ts
+++ src/app/edit-group-dialog/edit-group-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Group } from '../models/group';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-group-dialog',
@@ -8,17 +9,29 @@ import { Group } from '../models/group';
   styleUrls: ['./edit-group-dialog.component.css']
 })
 export class EditGroupDialogComponent {
+  editGroupForm: FormGroup;
+
   constructor(
     public dialogRef: MatDialogRef<EditGroupDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { group: Group }
-  ) {}
+    @Inject(MAT_DIALOG_DATA) public data: { group: Group },
+    private fb: FormBuilder
+  ) {
+    this.editGroupForm = this.fb.group({
+      name: [data.group.name, Validators.required],
+      description: [data.group.description],
+    });
+  }
 
   removeDevice(deviceId: string) {
     this.data.group.devices = this.data.group.devices.filter(device => device.id !== deviceId);
   }
 
   save() {
-    this.dialogRef.close(this.data.group);
+    if (this.editGroupForm.valid) {
+      this.data.group.name = this.editGroupForm.get('name')?.value;
+      this.data.group.description = this.editGroupForm.get('description')?.value;
+      this.dialogRef.close(this.data.group);
+    }
   }
 
   discard() {

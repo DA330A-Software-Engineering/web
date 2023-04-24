008448103d66f2978f0ef40eaa17d1381255e65c
Aki Sirkiä <74837175+lekatehdas@users.noreply.github.com>
2023-04-24 13:14:29 +0200

    Edit now can add devices of same type.

--- src/app/edit-group-dialog/edit-group-dialog.component.ts
+++ src/app/edit-group-dialog/edit-group-dialog.component.ts
@@ -1,7 +1,8 @@
-import { Component, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Group } from '../models/group';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {Component, Inject} from '@angular/core';
+import {MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {Group} from '../models/group';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {DeviceService} from '../service/deviceService/device.service';
 
 @Component({
   selector: 'app-edit-group-dialog',
@@ -10,17 +11,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class EditGroupDialogComponent {
   editGroupForm: FormGroup;
+  sameTypeDevices: { id: string; data: any }[] = [];
 
   constructor(
     public dialogRef: MatDialogRef<EditGroupDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { group: Group },
-    private fb: FormBuilder
-  ) {
+    private fb: FormBuilder,
+    private deviceService: DeviceService) {
     this.editGroupForm = this.fb.group({
       name: [data.group.name, Validators.required],
       description: [data.group.description],
     });
+    this.getAllSameTypeDevices(this.data.group.devices).then(devices => {
+      this.sameTypeDevices = devices;
+    });
   }
+
 
   removeDevice(deviceId: string) {
     this.data.group.devices = this.data.group.devices.filter(device => device.id !== deviceId);
@@ -34,7 +40,40 @@ export class EditGroupDialogComponent {
     }
   }
 
-  discard() {
+  async discard() {
     this.dialogRef.close();
   }
+
+  private async getDeviceTypes(): Promise<string[]> {
+    const devices = await this.deviceService.getAllDevices();
+    const deviceTypes = new Set(devices.map(device => device.data.type));
+    return Array.from(deviceTypes);
+  }
+
+  isDeviceInGroup(deviceName: string): boolean {
+    return this.data.group.devices.some(device => device.data.name === deviceName);
+  }
+
+  async getAllSameTypeDevices(groupDevices: string[] | any[]) {
+    const groupDeviceTypes = new Set(groupDevices.map(device => device.data.type));
+    const allDevices = await this.deviceService.getAllDevices();
+
+    return allDevices.filter(device => groupDeviceTypes.has(device.data.type));
+  }
+
+  thereAreDevicesNotInTheList(devices: string[] | any[]): boolean {
+    return this.sameTypeDevices.length !== devices.length;
+  }
+
+  async addDevice(deviceNameToAdd: string) {
+    if (!this.isDeviceInGroup(deviceNameToAdd)) {
+      const allDevices = await this.deviceService.getAllDevices();
+      const device = allDevices.find(device => device.data.name === deviceNameToAdd);
+
+      if (device) {
+        (this.data.group.devices as { id: string; data: any }[]).push(device);
+      }
+    }
+  }
+
 }

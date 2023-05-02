5e7a9d453e5b4c805fe7a9778d842e4a1a3ae650
dtrix90 <dominique_90_@hotmail.com>
2023-05-02 20:22:02 +0200

    removed commented out code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,7 +44,6 @@ const appRoute: Routes = [
   {path:'aboutus', component: AboutusComponent, canActivate:[AuthGuard]},
   {path:'login', component: LoginComponent},
   {path:'signup', component: SignupComponent},
-  //{path:'', component: LoginComponent},
   { path: 'sensors', component: SensorsComponent },
 ]
 
